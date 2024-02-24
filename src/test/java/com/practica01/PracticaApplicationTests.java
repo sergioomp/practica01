50e99f0b25bf8c1ee37588da3c02e675fc1ad3f1
Sergio Montenegro <smontenegroperez@gmail.com>
2024-02-24 16:00:00 -0600

    Creacion de proyecto, conección a base de datos y puerto

---
+++ src/test/java/com/practica01/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
